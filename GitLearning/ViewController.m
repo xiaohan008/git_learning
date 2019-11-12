6ed9cc5fa174ff9b36dbe05c61f957c6ed80e974
ly0889 <ly0889@163.com>
2019-11-12 17:41:45 +0800

    fix viewDidLoad 2222

--- GitLearning/ViewController.m
+++ GitLearning/ViewController.m
@@ -19,7 +19,7 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor lightGrayColor];
     self.title = @"首页";
-    NSLog(@"11111");
+    NSLog(@"22222");
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
