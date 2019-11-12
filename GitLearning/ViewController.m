ff85b639b1bfc34b781e078f846d5e37f68d5faa
yangyang <15010267362@163.com>
2019-11-12 17:44:02 +0800

    fix viewDidLoad 33333

--- GitLearning/ViewController.m
+++ GitLearning/ViewController.m
@@ -19,7 +19,7 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor lightGrayColor];
     self.title = @"首页";
-    NSLog(@"11111");
+    NSLog(@"33333");
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
